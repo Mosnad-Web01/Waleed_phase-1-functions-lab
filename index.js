c728c0b26af3954363aff245473a1ec3939959b5
waleed7333 <waleed733150@gmail.com>
2024-08-15 03:20:19 +0300

    add code of JS

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return distanceFromHqInFeet(destination) - distanceFromHqInFeet(start);
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; 
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25; 
+  } else {
+    return "cannot travel that far";
+  }
+}
+
+// console.log(calculatesFarePrice(43, 44));
+// console.log(calculatesFarePrice(50, 58));
+// console.log(calculatesFarePrice(34, 24));
